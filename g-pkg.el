cff28a4cfd392a8cf42658d8b24334e20e12002f
Gautam Mohan <me@gautammohan.com>
2025-10-06 15:53:07 -0400

    try

---
+++ g-pkg.el
@@ -0,0 +1,6 @@
+(define-package
+  "g"
+  "0.0.1"
+  "Generic Mode-Based Overhaul."
+  '((emacs "29.0")
+    (evil "1.15")))
---
+++ g.el
@@ -0,0 +1,19 @@
+;;; g.el --- Generic Mode-Based Overhaul
+
+;; Author: Gautam Mohan <me@gautammohan.com>
+;; Maintainers: Gautam Mohan <me@gautammohan.com>
+;; Created: 2025-10-06
+;; Version: 0.0.1
+;; Keywords: emulations
+;; URL: https://github.com/gautammohan/g-mode.el
+;;; Commentary:
+
+;; g.el or "Gmacs" is yet another overhaul of Emacs functionality similar to Spacemacs or Doom based on mneumonic, keyboard-driven modal interfaces.
+
+;;; Code:
+
+(defconst g-version "0.0.1")
+
+(provide 'g)
+
+;;; g.el ends here
