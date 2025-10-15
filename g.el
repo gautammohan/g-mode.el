fe80a8abade0f7ad10de89297dc59c5a10a1e9de
Gautam Mohan <me@gautammohan.com>
2025-10-15 00:42:50 -0400

    add font family variables with auto-refreshing of dependent faces

--- g.el
+++ g.el
@@ -14,6 +14,32 @@
 
 (defconst g-version "0.0.1")
 
+(defgroup g nil
+  "Gmacs Customizations"
+  :group 'emacs)
+
+
+(defface g-default nil
+  "Gmacs default face")
+
+(defun g--refresh-fonts (sym val)
+  "This function will reset all gmacs faces dependent on a g-font-family variable.
+
+NOTE: the variables are currently hardcoded in and this function must be updated whenever a new font family is added or another face redefines its font."
+  (set-default-toplevel-value sym val)
+  (set-face-attribute 'g-default nil :family g-font-family-monospaced)
+  ())
+
+(defcustom g-font-family-monospaced "Roboto Mono"
+  "Monospaced font for gmacs, must have light/medium/bold weights and roman/italic styles"
+  :type 'string
+  :set 'g--refresh-fonts)
+
+(defcustom g-font-family-proportional "Roboto"
+  "Variable-pitch font for gmacs, must have light/medium/bold weights, roman/italic styles, and condensed/normal widths"
+  :type 'string)
+
+
 (provide 'g)
 
 ;;; g.el ends here
