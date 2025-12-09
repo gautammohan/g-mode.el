6b978bd2f67dbb9c1a90a6c143dfc8ed6f91968a
Gautam Mohan <me@gautammohan.com>
2025-12-09 13:49:17 -0500

    due to weirdness when expanding the setf, factor out gss--setstyle
    also wrestled through the expansions so that one or two calls to
    gss-set/style/defface seem to be doing the correct things so i have
    uneasy confidence in the macrolet exprs inside of gss-with-palette

--- gss.el
+++ gss.el
@@ -61,14 +61,10 @@
     (hotaru-bi        . "#e7dc5f"))
   "Color values taken from Pilot's Iroshizuku Ink line + some custom additions")
 
-(defun gss--symcat (&rest symbols)
-  "Concatenate a list of symbols using \"-\""
-  (intern (string-join (mapcar #'symbol-name symbols) "-")))
-
 (defconst gss-spec-types '(color attr))
 
 ;; define a spec fragment which will ultimately be merged into a complete face. type must be a value specified by gss-spec-types (TODO). if palette is nil explicitly then return the uninterned spec symbol itself. TODO - also deal with non-interned palette symbols
-(cl-defmacro gss--defspec (name &key type gui tty (palette 'gss--spec))
+(cl-defmacro gss--defspec (name &key type gui tty (palette 'gss--global))
   `(let ((spec (gensym (format "%s:" (symbol-name ,type)))))
      (put spec 'gui ,gui)
      (put spec 'tty ,tty)
@@ -77,7 +73,6 @@
 
 ;; Reify a gss spec fragment into an actual Emacs face-spec. each spec must have its own function that provides relevant information.
 
-;; layer - :foreground or :background, variant is light or dark
 ;; reify all defined specs into base face defns and populate the palette namespace
 (cl-defun gss--reify (spec face-sym context)
   ;; for each spec
@@ -89,16 +84,16 @@
                            (symbol-name (get spec 'type)))))
          (result (funcall reifier spec context)))
     (if (hash-table-p result)
-        (map-apply (lambda (id spec) (let ((face (gss--symcat face-sym id)))
+        (map-apply (lambda (id spec) (let ((face (intern (string-join (mapcar #'symbol-name (list face-sym id)) "-"))))
                                        (face-spec-set face spec)
                                        (cons id face)))
                    result)
       (progn (face-spec-set face-sym result)
              face-sym))))
 
-;; layer - :foreground or :background, variant is light or dark
-(cl-defun gss--reify/color (spec variant)
-  (let* ((extract (cond
+(cl-defun gss--reify/color (spec context)
+  (let* ((variant (alist-get 'variant context))
+         (extract (cond
                    ((eq variant 'light) #'car)
                    ((eq variant 'dark) #'cdr)
                    (t (progn 
@@ -112,11 +107,12 @@
 
     (map-into result 'hash-table)))
 
-(cl-defun gss--reify/attr (spec variant)
+(cl-defun gss--reify/attr (spec context)
   `((((type graphic)) . ,(get spec 'gui))
     (((type tty)) . ,(get spec 'tty))))
-gss--reify/attr
 
+(cl-defun gss--setstyle (style palette)
+  (setf (alist-get style (alist-get 'style palette))  style))
 
 (cl-defun gss--update-palettes (context &key (palettes '(gss--global)))
   (cl-loop for palette in palettes do
@@ -132,11 +128,11 @@ gss--reify/attr
                            (set-face-attribute ,face nil :inherit (list ,@styles))))
                  (gss-defface (face doc &rest styles)
                    `(progn (defface ,face nil ,doc)
-                           (gss-set ,face ,@styles)))
+                           (gss-set ',face ,@styles)))
                  (gss-defstyle (face &rest styles)
-                   `(progn (gss-defface ,face nil "GSS Internal Style Def")
-                           (setf (alist-get ,face ,,palette) ,face))))
-     (let-alist ,palette
+                   `(progn (gss-defface ,face "GSS Internal Style Def" ,@styles)
+                           (gss--setstyle ',face ',,palette))))
+     (let-alist (alist-get 'style ,palette)
        ,@body)))
 
 (cl-defmacro gss-set (&rest body)
@@ -147,119 +143,5 @@ gss--reify/attr
 
 (cl-defmacro gss-defstyle (&rest body)
   `(gss-with-palette gss--global (gss-defstyle ,@body)))
-
-
-
-(defconst gss--variant 'light)
-
-(defconst gss--attributes nil)
-
-(cl-defmacro gss-group (name &key (colors nil) (styles nil) (relative nil))
-
-  )
-'(group ((color ((bar . blah) (baz . bloop)))
-         (face . ((default . zap) (emph)))))
-(gss-face 'foo :fg .bar :bg .baz :decor )
-
-(defface g--default nil "Default Gmacs Face")
-
-(defconst gss--colors nil)
-(cl-defmacro gss-color (name light dark &key (palette gss--default-palette))
-  `(let-alist ',palette
-     (setf (alist-get 'light (alist-get ,name gss--colors)) ,light)
-     (setf (alist-get 'dark (alist-get ,name gss--colors)) ,dark)))
-
-(gss-color 'text-body .syun-gyo .timberwolf)
-(gss-color 'text-subtle .ina-ho .kiri-same)
-(gss-color 'ui-base .ivory .take-sumi)
-
-(defconst gss--props nil)
-(cl-defmacro gss-prop (group name face-attr)
-  (setf (alist-get ,name (alist-get ,group gss--props)) ,face-attr))
-(gss-prop 'font 'size '())
-
-(cl-defmacro gss-face (name group &rest blah &key (fg nil) (bg nil))
-  (format "name: %s group: %s blah: %s fg: %s bg: %s" name group blah fg bg))
-
-
-;; Default theme colors use Nord
-(defconst gss-attributes
-  `((font . ((size . ,(defface gss--attr-font-size '((default . (:height 160))) "Gmacs internals -- DO NOT EDIT"))
-             (monospace . ,(defface gss--attr-font-monospace '((default . (:family "Roboto Mono"))) "Gmacs internals -- DO NOT EDIT"))
-             (proportional . ,(defface gss--attr-font-proportional '((default . (:family "Roboto"))) "Gmacs internals -- DO NOT EDIT"))
-             (straight . ,(defface gss--attr-font-straight '((default . (:slant normal))) "Gmacs internals -- DO NOT EDIT"))
-             (italic . ,(defface gss--attr-font-italic '((default . (:slant italic))) "Gmacs internals -- DO NOT EDIT"))
-             (regular . ,(defface gss--attr-font-regular '((default . (:weight regular))) "Gmacs internals -- DO NOT EDIT"))
-             (emph . ,(defface gss--attr-font-emph '((default . (:weight bold))) "Gmacs internals -- DO NOT EDIT"))
-             (normal . ,(defface gss--attr-font-normal '((default . (:width regular))) "Gmacs internals -- DO NOT EDIT"))))
-    (mod . ((extend . ,(defface gss--attr-mod-extend '((default . (:extend t))) "Gmacs internals -- DO NOT EDIT"))))))
-
-(cl-defmacro gss-set (face &rest style)
-  "Set a face to obey the style attrs specified. style arguments may
-be attrs or mixins. Note the order of the attributes matters, with
-earlier items taking precedence over later ones."
-  ;; This macro interprets each of the dotted style accessors in the context of the gss-attributes let-alist and produces a quoted list of faces to pass to :inherit. Note that we need to use ":inherit (list ,@style)" instead of ":inherit ,style" because set-face-attribute would try to evaluate the expanded ,style form literally as a function, which we don't want. This same technique is used in gss-mixin to "defer" list construction to runtime.
-  `(let-alist gss-attributes
-     (face-spec-reset-face ,face)
-     (set-face-attribute ,face nil :inherit (list ,@style))))
-
-(defun gss-unset (face)
-  "Restore previous attributes of face and make it no longer dependent
-on GSS style attrs"
-  (unless (eq face 'default)
-    (face-spec-reset-face face)
-    ;; Note: this is a nonstandard way to restore the original attrs of the face.
-    ;; We are not using *-reset face functions since we are not modifying any of the property values
-    ;; associated with existing emacs machinery for custom themes, faces, or specs.
-    (apply #'set-face-attribute face nil (face-spec-choose (get face 'face-defface-spec)))))
-
-(cl-defmacro gss-mixin (face docstr &rest style)
-  "Define a \"mixin\" face using the provided style attributes. This
-  must be a unique face name, preferably prefixed by \"gss-\". It can be passed to other gss-*
-  functions as a style argument. Due to face inheritance semantics, a
-  mixin should be at the end of the style attr list so it can be
-  overridden by explicit styles."
-  ;; This code constructs a defface expression with one face spec: "default". Each style argument is interpreted as "dotted access" of the gss-attributes nested alists. The macro is constructed carefully to ensure the "dotted accessors" are interpreted at runtime so they are meaningful in the context of (let-alist gss-attributes ...) while producing a form to match the face spec of ((SPEC . ATTRLIST)).
-  ;; An example expansion:
-  ;; (gss-mixin gss-highlight "Highlight background shade" .color.ui.secondary .mod.extend)
-  ;; (defface gss-highlight `((default . (:inherit (,.color.ui.secondary
-  ;;                                              ,.mod.extend)))) "Highlight background shade")
-  `(let-alist gss-attributes
-     (defface ,face (list  (cons 'default (list :inherit (list  ,@style)))) ,docstr :group 'g-faces)))
-
-(defun gss--sync-default ()
-  "This function syncs the \"default\" Emacs face to \"gss-default\", the
-defined Gmacs default face which respects underlying gss attributes.
-Whenever an attribute inherited by gss-default is updated, this
-function must be called, since \"default\" is the face which ultimately
-provides values for all unspecified attributes."
-  (cl-loop for (attr . name) in face-attribute-name-alist do
-           (set-face-attribute 'default nil attr (face-attribute 'gss-default attr nil t))
-           ;; We need to redisplay between each call to set an attribute to avoid issues with batching
-           ;; font spec (:height :slant :weight etc..) and font family changes. There are probably more
-           ;; efficient ways to do it (first changing family, then the spec attrs) but this works for now
-           (redisplay)))
-
-;; The "gss-default" face is constructed by hand for now since it must be fully specified (so we can sync the default face attrs from it). Currently gss cannot pass "raw" face attributes via its definition functions, so we must do it this way.
-(let-alist gss-attributes
-  (defface gss-default `((default . (
-                                   :foundry nil
-                                   :underline nil
-                                   :strike-through nil
-                                   :box nil
-                                   :inverse-video nil
-                                   :stipple nil
-                                   :extend nil
-                                   :inherit (,.font.size
-                                             ,.font.monospace
-                                             ,.font.regular
-                                             ,.font.straight
-                                             ,.font.normal
-                                             ,.color.text.base
-                                             ,.color.ui.background)))) "Default props for frame text"))
-(gss--sync-default)
-
-(gss-mixin gss-highlight "Prominent Highlight" .color.ui.secondary .mod.extend)
-(gss-set 'region .color.ui.secondary .mod.extend)
 
 (provide 'gss)
